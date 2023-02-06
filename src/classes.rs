b8c42517a41175a3ff712bb563325942421eba13
Dex <sheepareawesome777@gmail.com>
2023-02-06 12:44:49 -0500

    neural net struct stuff

--- src/classes.rs
+++ src/classes.rs
@@ -2,3 +2,35 @@
 // stuff that would take up too much space in main
 
 pub type IDFC<T> = Result<T, Box<dyn std::error::Error>>;
+
+pub struct NeuralNet {
+	hidden_layers:	Vec<NeuralNetLayer>
+}
+
+pub struct NeuralNetLayer {
+	neurons:	Vec<NeuralNetNode>
+}
+
+// the "neurons" of the brain
+pub struct NeuralNetNode {
+	// (m, b)
+	weight:	(f64, f64),
+	offset: (f64, f64),
+}
+
+impl NeuralNetNode {
+	pub fn result(&self) {
+		// calculate signal to send forward
+	}
+
+	pub fn apply_offset(&mut self) {
+		self.weight = (
+			self.weight.0 + self.offset.0,
+			self.weight.1 + self.offset.1,
+		);
+
+		self.offset = (
+			0.0, 0.0
+		);
+	}
+}
--- src/sub.rs
+++ src/sub.rs
@@ -2,10 +2,14 @@
 * Subcommands
 */
 
+use std::fs;
+
+// train command code
 pub fn train(m_per_gen: u32) {
-	//
+	let items = fs::read_dir("./dataset");
 }
 
+// test a single image
 pub fn feed_img() {
 	//
 }
